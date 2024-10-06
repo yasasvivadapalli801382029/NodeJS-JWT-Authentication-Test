8e675bc9e494a41f999f5c054163231d63ff31d0
yasasvivadapalli801382029 <yasasvi09@gmail.com>
2024-10-06 13:03:49 -0400

    modified according to instructions

--- server.js
+++ server.js
@@ -1,101 +1,110 @@
 const express = require('express');
 const app = express();
-const jwt = require("jsonwebtoken");
-const { expressjwt } = require('express-jwt');
+
+const jwt = require('jsonwebtoken');
+const { expressjwt: expressJwt } = require('express-jwt');
 const bodyParser = require('body-parser');
 const path = require('path');
 
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-    res.setHeader("Access-Control-Allow-Headers", "Content-type,Authorization");
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-type,Authorization');
     next();
 });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const port = 3000;
 
-const PORT = 3000;
 const secretKey = 'My super secret key';
-const jwtMW = expressjwt({
+const jwtMW = expressJwt({
     secret: secretKey,
-    algorithms: ["HS256"],
+    algorithms: ['HS256']
 });
+
 
 
 let users = [
     {
         id: 1,
-        username: "yasasvi",
-        password: "123",
+        username: 'yasasvi',
+        password: '123'
     },
     {
         id: 2,
-        username: "vadapalli",
-        password: "456",
-    },
+        username: 'vadapalli',
+        password: '456'
+    }
 ];
 
-app.post("/api/login", (req, res) => {
+
+app.post('/api/login', (req, res) => {
     const { username, password } = req.body;
+    let token = null;
+    let userFound = false; 
 
     for (let user of users) {
-        if (username == user.username && password == user.password) {
-            let token = jwt.sign(
-                { id: user.id, username: user.username },
-                secretKey,
-                { expiresIn: "180s" }
-            );
+        if (username === user.username && password === user.password) {
+            token = jwt.sign({ id: user.id, username: user.username }, secretKey, { expiresIn: '3m' });
             res.json({
                 success: true,
                 err: null,
-                token,
+                token
             });
-            found = true;
-            break;
+            userFound = true; 
+            break; 
         }
+    }
 
-        else {
-            res.status(401).json({
-                success: false,
-                token: null,
-                err: "Username or Password is incorrect",
-            });
-        }
+    if (!userFound) {
+        res.status(401).json({
+            success: false,
+            token: null,
+            err: 'Username or password is incorrect'
+        });
     }
 });
 
-app.get("/api/dashboard", jwtMW, (req, res) => {
+app.get('/api/dashboard', jwtMW, (req, res) => {
     res.json({
         success: true,
-        myContent: "Secret content that only logged in people can see!!!",
+        myContent: 'Secret content that only logged in people can see!!!'
     });
 });
 
-app.get("/api/prices", jwtMW, (req, res) => {
+app.get('/api/prices', jwtMW, (req, res) => {
     res.json({
         success: true,
-        myContent: "This is the Price $3.99",
+        myContent: 'This is the price $3.99'
     });
 });
 
+//added route called settings
+app.get('/api/settings', jwtMW, (req, res) => {
+    res.json({
+        success: true,
+        myContent: 'This is settings page'
+    });
+});
 
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "index.html"));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.use(function (err, req, res, next) {
-    if (err.name == "UnauthorizedError") {
+    if (err.name === 'UnauthorizedError') {
         res.status(401).json({
             success: false,
             officialError: err,
-            err: "Username or password is incorrect 2",
+            err: 'Username or password is incorrect 2'
         });
-    } else {
+    }
+    else {
         next(err);
     }
 });
 
-app.listen(PORT, () => {
-    console.log(`Serving on port ${PORT}`);
+app.listen(port, () => {
+    console.log(`Serving on port ${port}`);
 });
